21e6fa2eb1b70960c2aa5b14218060942aa02360
marco.argnani <marco.argnani@ximplia.it>
2024-10-14 18:01:48 +0200

    Aggiunta gestione API con autenticazione

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\UserController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +19,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/* Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+}); */
+
+Route::middleware('auth:sanctum')->get('/user', [UserController::class, 'index']);
+
+
+
+Route::post('/login', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        return response()->json(['message' => 'Invalid credentials'], 401);
+    }
+
+    // Creare il token e restituirlo come risposta
+    $token = $user->createToken('API Token')->plainTextToken;
+
+    return response()->json(['token' => $token]);
 });
--- routes/web.php
+++ routes/web.php
@@ -19,18 +19,19 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/admin', function () {
+
+/* Route::get('/admin', function () {
     return view('admin');
-})->middleware(['auth', 'verified'])->name('admin');
+})->middleware(['auth', 'verified'])->name('admin'); */
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::get('/prova', [xSimpleController::class, 'index'])->name('xcrud');
-  /*   Route::get('/admin', function () {
+    Route::get('/admin', function () {
         return view('admin');
-    })->name('admin'); */
+    })->name('admin');
 });
 
 // TEST PER CREAZIONE PDF
